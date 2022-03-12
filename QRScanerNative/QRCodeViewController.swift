214af7b893bcc8a731d5ff74f8f97cc7c7c6cffd
Andrei Bezlepkin <6abez6@gmail.com>
2022-03-12 11:22:49 +0300

    init

--- QRScanerNative/QRCodeViewController.swift
+++ QRScanerNative/QRCodeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class QRCodeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ QRScanerNative/QRScannerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QRScannerViewController.swift
+//  QRScanerNative
+//
+//  Created by Andrei Bezlepkin on 12.03.22.
+//
+
+import UIKit
+
+class QRScannerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
